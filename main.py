4620e8ee602e7c13c4f8b244d08b2a01c4cacc32
Jose S. Daniel <djose1164@gmail.com>
2021-07-24 19:11:17 -0400

    Palette added

--- main.py
+++ main.py
@@ -5,12 +5,26 @@ from kivy.utils import get_color_from_hex
 from kivy.base import EventLoop
 from kivy.graphics import Line, Color
 from kivy.lang import Builder
+from kivy.uix.behaviors import ToggleButtonBehavior
+from kivy.uix.togglebutton import ToggleButton
 import os
 
 kv_path: str = "./kv/"
 for kv in os.listdir(kv_path):
     Builder.load_file(kv_path + kv)
 
+class RadioButton(ToggleButton):
+    """A toggle button with the characteristic of a radio button. I mean, one
+    button must be down.
+
+    Args:
+        ToggleButton (ToggleButton): The parent class for this.
+    """
+    def _do_press(self):
+        """For keeping a button down always.
+        """
+        if self.state == "normal":
+            ToggleButtonBehavior._do_press(self)
 
 class CanvasWidget(Widget):
     def __init__(self, **kwargs):
@@ -44,9 +58,24 @@ class CanvasWidget(Widget):
         self.canvas.clear()
         for widget in saved:
             self.add_widget(widget)
+    
+    def set_color(self, new_color):
+        """Change the current color to a new color.
+
+        Args:
+            new_color (Color): The color to use.
+        """
+        print(new_color)
+        with self.canvas:
+            Color(*new_color)
 
 
 class PaintApp(App):
+    """Paint App. This the app class.
+
+    Args:
+        App (App): The app what's beeing executing.
+    """
     def build(self):
         EventLoop.ensure_window()
         if EventLoop.window.__class__.__name__.endswith("Pygame"):
@@ -58,7 +87,10 @@ class PaintApp(App):
                 mouse.set_cursor((24, 24), (9, 9), a, b)
             except:
                 pass
-        return CanvasWidget()
+        
+        self.canvas_widget = CanvasWidget()
+        self.canvas_widget.set_color(get_color_from_hex("#2980B9"))
+        return self.canvas_widget
 
 
 if __name__ == "__main__":
